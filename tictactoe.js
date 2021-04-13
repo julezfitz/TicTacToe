23bb2fe6e2da45083cc2bbcbc6b31de5da7f5a81
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-04-13 13:23:24 -0230

    Added one player game option with computer player. Set name fields to required where needed.

--- tictactoe.js
+++ tictactoe.js
@@ -49,7 +49,7 @@ TicTacToe.GameBoard = function (logger) {
                     return "O";
                 }
             } else if (spaces[0][1] != '-' && ((spaces[0][1] === spaces[1][1]) && (spaces[0][1] === spaces[2][1]))) {
-                    console.log("win");
+                console.log("win");
                 if (spaces[0][1] === "X") {
                     return "X";
                 } else if (spaces[0][1] === "O") {
@@ -91,6 +91,17 @@ TicTacToe.GameBoard = function (logger) {
                 }
             }
         },
+        possibleMoves: function () {
+            let legalMoves = [];
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    if (spaces[i][j] === "-") {
+                        legalMoves.push([i, j]);
+                    };
+                };
+            };
+            return legalMoves;
+        },
         getCurrentBoardState: function () {
             let gameStateCopy = spaces.map(function (arr) {
                 return arr.slice();
@@ -100,12 +111,13 @@ TicTacToe.GameBoard = function (logger) {
     }
 }(console);
 
-TicTacToe.createPlayer = function (name) {
+TicTacToe.createPlayer = function (name, isComputer) {
     return {
         name,
         wins: 0,
         losses: 0,
-        ties: 0
+        ties: 0,
+        isComputer, 
     };
 }
 
@@ -121,6 +133,11 @@ TicTacToe.GameController = function () {
             _currentPlayer = _playerX = playerX;
             _playerO = playerO;
             console.log(_playerX);
+
+            if(_currentPlayer.isComputer) {
+                const computerTurn = TicTacToe.GameController.computerTurn();
+                TicTacToe.GameController.takeTurn(computerTurn[0], computerTurn[1]);
+            };
         },
         whoseTurn: function () {
             return _currentPlayer;
@@ -136,10 +153,19 @@ TicTacToe.GameController = function () {
                     }
                 } else {
                     _currentPlayer = (_currentPlayer == _playerX) ? _playerO : _playerX;
+                    if(_currentPlayer.isComputer) {
+                        const computerTurn = TicTacToe.GameController.computerTurn();
+                        TicTacToe.GameController.takeTurn(computerTurn[0], computerTurn[1]);
+                    };
                 }
                 return true;
             }
             return false;
+        },
+        computerTurn: function () {
+            let possibleMoves = TicTacToe.GameBoard.possibleMoves();
+            let randomMove = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+            return randomMove;
         },
         isGameOver: function () {
             return TicTacToe.GameBoard.isGameOver();
@@ -207,7 +233,7 @@ TicTacToe.DisplayController = function () {
             });
         },
         pageLoadHandler: function () {
-            if(player1 === undefined) {
+            if (player1 === undefined) {
                 this.showCreatePlayers();
                 this.createPlayers();
                 this.hidePopUp();
@@ -247,37 +273,52 @@ TicTacToe.DisplayController = function () {
             againText = document.getElementById("againText");
             againText.setAttribute('style', 'white-space: pre;');
             if (TicTacToe.GameController.winner()) {
-            againText.textContent = TicTacToe.GameController.winner().name + ' wins! \r\n';
-            againText.textContent += player1.name + ":  Wins: " + player1.wins + "  Losses: " + player1.losses + "  Ties: " + player1.ties + "\r\n";
-            againText.textContent += player2.name + ":  Wins: " + player2.wins + "  Losses: " + player2.losses + "  Ties: " + player2.ties + "\r\n";
-        } else
-            {againText.textContent = 'Tie!';
-        };
+                againText.textContent = TicTacToe.GameController.winner().name + ' wins! \r\n';
+                againText.textContent += player1.name + ":  Wins: " + player1.wins + "  Losses: " + player1.losses + "  Ties: " + player1.ties + "\r\n";
+                againText.textContent += player2.name + ":  Wins: " + player2.wins + "  Losses: " + player2.losses + "  Ties: " + player2.ties + "\r\n";
+            } else {
+                againText.textContent = 'Tie!';
+            };
         },
         hideEndPopUp: function () {
             endMenu = document.getElementById("endGame");
             endMenu.style.display = 'none';
         },
-        createPlayers: function() {
+        createPlayers: function () {
             const nameInputPlayer1 = document.getElementById("nameInputPlayer1");
             const nameInputPlayer2 = document.getElementById("nameInputPlayer2");
             
             let playersButton = document.getElementById("submit");
 
-            playersButton.addEventListener("click", function() {
+            playersButton.addEventListener("click", function () {
+                if((nameInputPlayer1.value.length < 1) || (nameInputPlayer2.value.length < 1)) {
+                    alert ("Please submit player names.");
+                } else {
                 player1 = TicTacToe.createPlayer(nameInputPlayer1.value);
                 player2 = TicTacToe.createPlayer(nameInputPlayer2.value);
                 TicTacToe.DisplayController.showPopUp();
                 TicTacToe.DisplayController.hideCreatePlayers();
                 console.log(nameInputPlayer1);
-              });
-            
+                }
+            });
+
+            let soloButton = document.getElementById("soloGame");
+            soloButton.addEventListener("click", function() {
+                if(nameInputPlayer1.value.length < 1) {
+                    alert ("Please submit player names.");
+                } else {
+                player1 = TicTacToe.createPlayer(nameInputPlayer1.value);
+                player2 = TicTacToe.createPlayer("Computer", true);
+                TicTacToe.DisplayController.showPopUp();
+                TicTacToe.DisplayController.hideCreatePlayers();
+                }  
+            });
         },
-        showCreatePlayers: function() {
+        showCreatePlayers: function () {
             startMenu = document.getElementById("setPlayerNames");
             startMenu.style.display = 'block';
         },
-        hideCreatePlayers: function() {
+        hideCreatePlayers: function () {
             startMenu = document.getElementById("setPlayerNames");
             startMenu.style.display = 'none';
         }
